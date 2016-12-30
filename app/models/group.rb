efe236727b097634b90adae7efbc081d218d533e
jian <xiaoshuxixi@gmail.com>
2016-12-31 01:12:51 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
+  has_many :group_relationships
+  has_many :members, through: :group_relationships, source: :user
 end
